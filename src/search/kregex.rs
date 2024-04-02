30b1c3ab13bea76b12bba8c1f5b758beec36bbc6
LugeK1t <263635591@qq.com>
2024-04-03 01:02:24 +0800

    fix: ignore case in regex search

--- src/search/kregex.rs
+++ src/search/kregex.rs
@@ -19,11 +19,19 @@ impl Search for KRegex {
         param.contents.iter()
             .enumerate()
             .filter_map(|(i, &line)| {
-                let highlights: Vec<_> = re.find_iter(&line.to_lowercase())
-                    .map(|m| {
-                        (m.start(), m.end())
-                    })
-                    .collect();
+                let highlights: Vec<_> = if param.ignore_case {
+                    re.find_iter(&line.to_lowercase())
+                        .map(|m| {
+                            (m.start(), m.end())
+                        })
+                        .collect()
+                } else {
+                    re.find_iter(line)
+                        .map(|m| {
+                            (m.start(), m.end())
+                        })
+                        .collect()
+                };
 
                 if highlights.len() > 0 {
                     Some(Ok(SearchResult::new(i, highlights)))
